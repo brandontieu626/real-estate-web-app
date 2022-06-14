83e9bdbf3879f759c81b37ea8f7779959544db20
brandontieu626 <58931427+brandontieu626@users.noreply.github.com>
2022-06-14 14:27:27 -0700

    Adding Cover Photos, API Calls
    Added cover photos for houses, made API Calls with Axios, better formatting

---
+++ utils/fetchApi.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+export const baseUrl='https://bayut.p.rapidapi.com'
+
+
+
+export const fetchApi = async(url) =>{
+    const {data} = await axios.get((url), {
+        headers: {
+            'X-RapidAPI-Key': '9da0c30f96msh6c9c58d42112a06p1bea7djsn913e5690fa24',
+            'X-RapidAPI-Host': 'bayut.p.rapidapi.com'
+        }
+    });
+
+    return data;
+}
