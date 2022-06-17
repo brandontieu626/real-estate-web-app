051ed9ccf97d9eba617a095c9a899c09b4515b22
brandontieu626 <58931427+brandontieu626@users.noreply.github.com>
2022-06-17 14:14:45 -0700

    Search,Scroll,Details
    Added search pages,scrolling for images, and a details page

---
+++ components/Layout.jsx
@@ -0,0 +1,26 @@
+import Head from 'next/head';
+import {Box} from '@chakra-ui/react'
+
+import Navbar from "../components/Navbar"
+import Footer from "../components/Footer"
+const Layout = ({children}) =>(
+    <>
+        <Head>
+            <title>ProRealtors</title>
+        </Head>
+        <Box maxWidth="1280px" m="auto">
+            <header>
+                <Navbar/> 
+            </header>
+            <main>
+                {children}
+            </main>
+            <footer>
+                <Footer/>
+            </footer>
+
+        </Box>
+    </>
+)
+
+export default Layout;
